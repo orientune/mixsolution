3c4cff8f7415d5c073a044e4efece99c3a67f993
Murat TUNA <41265396+orientune@users.noreply.github.com>
2023-12-05 16:29:38 +0100

    MultidimensionalArrays.java

---
+++ MultidimensionalArrays.java
@@ -0,0 +1,13 @@
+public class Main {
+   public static void main(String[] args) {
+       int[][] matrix = {
+               {8, 1, 6},
+               {3, 5, 7},
+               {4, 9, 0},
+       };
+      for(int[] row:matrix){
+          for(int a:row){System.out.println(a);};
+      }
+       
+   }
+}
